ae375702af06522752b8109fbe6aa9e2530c570d
BryanHogan <nayrb@gmx.de>
2023-08-09 20:56:53 +0900

    added postcss nano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require("postcss-preset-env")({
             stage: 1,
-        })
+        }),
+        require("cssnano")({ preset: "default" }),
     ]
 }
