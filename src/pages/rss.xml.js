dbd958310012b15194ba0b4cb3a2f07051c9baaf
BryanHogan <nayrb@gmx.de>
2023-08-28 22:08:28 +0900

    footer fixed & extra infos blog post

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function get() {
   return rss({
     title: "Bryan Hogan's Page",
-    description: "Blog about nice stuff and more.",
+    description: "Bryan's blog about creating, learning and finding balance.",
     site: "https://bryanhogan.com",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: "<language>en-us</language>",
