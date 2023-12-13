6743f4693d359bcba38bbcb175ca9338d07865c1
BryanHogan <nayrb@gmx.de>
2023-12-13 17:05:15 +0900

    added validation to zod schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,9 @@ import { z, defineCollection } from "astro:content";
 const postsCollection = defineCollection({
     type: 'content',
     schema: z.object({
-        title: z.string(),
+        title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         author: z.string(),
-        description: z.string(),
+        description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
         coverImage: z.object({
             url: z.string(),
             alt: z.string(),
