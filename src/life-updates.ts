7ebdd23f7e46f4eded06fcc32c5b05b09612f65e
BryanHogan <nayrb@gmx.de>
2024-01-06 17:48:29 +0900

    added life updates section to index page

---
+++ src/life-updates.ts
@@ -0,0 +1,26 @@
+interface LifeUpdate {
+  title: string;
+  date: Date;
+  description: string;
+  cardButtons: Array<CardLUButton>;
+}
+export interface CardLUButton {
+  buttonText: string;
+  buttonLink: string;
+  buttonTarget: string;
+}
+
+export const LifeUpdates: LifeUpdate[] = [
+  {
+    title: "New Years in Seoul",
+    date: new Date("2024-01-01"),
+    description: "Celebrating the start of the new year in Seoul.",
+    cardButtons: [
+      {
+        buttonText: "Instagram Highlight",
+        buttonLink: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE4MDE0MDk4NTExMDg1NTUw?igsh=eHJkNzMxNG1oODA3",
+        buttonTarget: "_blank",
+      },
+    ]
+  },
+];
