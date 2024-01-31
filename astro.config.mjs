40555ce978bb957045281a8a8c38849de62e2784
BryanHogan <nayrb@gmx.de>
2024-01-31 19:31:47 +0100

    astro build config fixed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.bryanhogan.com'
+    site: 'https://bryanhogan.com'
 });
 
 
