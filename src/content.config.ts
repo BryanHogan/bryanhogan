fdc06dc59430db1482013efc743c4c5a2e2af1b9
BryanHogan <bryanhoganme@gmail.com>
2025-02-07 14:55:04 +0100

    svg images in content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,10 +4,10 @@ import { glob, file } from 'astro/loaders';
 
 const blog = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
-        coverImage: z.string(), //maybe needs to change
+        coverImage: image(),
         pubDate: z.date(),
         tags: z.array(z.string())
     })
--- src/content/blog/astro-obsidian.md
+++ src/content/blog/astro-obsidian.md
@@ -1,7 +1,7 @@
 ---
 title: How To Use Obsidian To Write Astro Markdown Content
 description: The efficient way to edit your Astro project's markdown blog posts with Obsidian.
-coverImage: /post-covers/Obsidian-Astro-Cover.svg
+coverImage: ../blog-assets/covers/Astro-Obsidian-Cover.svg
 pubDate: 2024-06-07T10:51:52Z
 tags:
   - astro
