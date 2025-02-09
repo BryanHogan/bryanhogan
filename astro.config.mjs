b8df975cb37cae5fd9b414e4bf8a23cead9d4ff2
BryanHogan <bryanhoganme@gmail.com>
2025-02-09 12:20:46 +0100

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bryanhogan.com'
+  site: 'https://bryanhogan.com',
+  integrations: [sitemap()]
 });
