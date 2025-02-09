89c652384a8dc280f34f9c690a9fc33e9124bb8e
BryanHogan <bryanhoganme@gmail.com>
2025-02-09 12:18:01 +0100

    rss feed added

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: "Bryan's Blog",
+    description: "About learning, exploring and creating.",
+    site: context.site,
+    trailingSlash: false,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}`,
+    })),
+  })
+}
