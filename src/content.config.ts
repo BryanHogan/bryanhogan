56793e751a75f0ae79c6172e0b510825fa93fa58
BryanHogan <bryanhoganme@gmail.com>
2025-02-11 00:09:50 +0100

    can use emojis as coverImage now for blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,10 +7,14 @@ const blog = defineCollection({
     schema: ({ image }) => z.object({
         title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
-        coverImage: image(),
+        coverImage: image().optional(),
+        emoji: z.string().optional(),
         pubDate: z.date(),
         lastUpdate: z.date().optional(),
         tags: z.array(z.string())
+    }).refine((data) => data.coverImage || data.emoji, {
+        message: "Either coverImage or emoji must be provided.",
+        path: ["coverImage", "emoji"]
     })
 });
 
