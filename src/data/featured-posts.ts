2f23270c4fb93eaf1d52a298abeec9e56ad05c2d
bryanhogan <bryanhoganme@gmail.com>
2025-09-19 17:03:15 +0900

    compact-able BlogCard and index update

---
+++ src/data/featured-posts.ts
@@ -0,0 +1,12 @@
+/**
+ * Post IDs correspond to the markdown filenames in src/content/blog/
+ * For example: "introduction.md" -> "introduction"
+ */
+
+export const featuredPosts = [
+	"obsidian-zettelkasten", 
+	"south-korea-travel-guide",
+	"living-in-macau"
+];
+
+export default featuredPosts;
