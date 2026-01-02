53526a81fa23931cdfa89e5c11171bb94cd9fcdf
BryanHogan <bryanhoganme@gmail.com>
2026-01-02 22:06:03 +0900

    Update submodule + misc: max title length for posts increased to 65 from 60

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,7 @@ import { glob, file } from 'astro/loaders';
 const blog = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
     schema: ({ image }) => z.object({
-        title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
+        title: z.string().max(65, "Maximum of 65 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
         coverImage: image().optional(),
         emoji: z.string().optional(),
